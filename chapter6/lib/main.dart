a7c4e46c5ab5b33a9108ac3fbcdee16a21c42c92
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2025-02-14 21:16:06 +0900

    l10n

--- chapter6/lib/main.dart
+++ chapter6/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_editor/start_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: L10n.localizationsDelegates,
+      supportedLocales: L10n.supportedLocales,
       title: 'Edit Snap',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
