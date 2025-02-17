6ce348762dbc14086adf7eac0f272f8b2b8e1c45
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2025-02-17 10:32:35 +0900

    画像選択

--- chapter6/lib/image_select_screen.dart
+++ chapter6/lib/image_select_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as image_lib;
 
 class ImageSelectScreen extends StatefulWidget {
   const ImageSelectScreen({super.key});
@@ -9,9 +12,41 @@ class ImageSelectScreen extends StatefulWidget {
 }
 
 class _ImageSelectScreenState extends State<ImageSelectScreen> {
+  final ImagePicker _picker = ImagePicker();
+  // 8bit符号なし整数のリスト
+  Uint8List? _imageBitmap;
+
+  Future<void> _selectImage() async {
+    final XFile? imageFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+
+    final imageBitmap = await imageFile?.readAsBytes();
+    assert(imageBitmap != null);
+    if (imageBitmap == null) return;
+
+    final image = image_lib.decodeImage(imageBitmap);
+
+    assert(image != null);
+    if (image == null) return;
+
+    final image_lib.Image resizedImage;
+    if (image.width > image.height) {
+      // 横長の画像の場合、幅を500にリサイズ
+      resizedImage = image_lib.copyResize(image, width: 500);
+    } else {
+      // 縦長の画像の場合、高さを500にリサイズ
+      resizedImage = image_lib.copyResize(image, height: 500);
+    }
+
+    setState(() {
+      _imageBitmap = Uint8List.fromList(image_lib.encodeBmp(resizedImage));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
+    final imageBitmap = _imageBitmap;
 
     return Scaffold(
       appBar: AppBar(
@@ -22,8 +57,9 @@ class _ImageSelectScreenState extends State<ImageSelectScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            if (imageBitmap != null) Image.memory(imageBitmap),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () => _selectImage(),
               child: Text(l10n.imageSelect),
             ),
             ElevatedButton(
